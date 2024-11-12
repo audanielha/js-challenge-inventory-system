904f13b87b0593284c7da7f81f4c2b4034d2e15c
Daniel Ha <audanielha@gmail.com>
2024-11-12 22:27:23 +1300

    add add product function

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 let container = [
-    {"name" : "test"},
-    {"quantity": 0},
-    {"price": 0}
+    {
+    "name" : "test",
+    "quantity": 0,
+    "price": 0
+    }
 ]
 
-console.log(container[2])
+let addProduct = (name, quantity, price)=>{
+    container.push({name, quantity, price})
+}
+addProduct("test", 23, 23)
+console.log(container[1])
