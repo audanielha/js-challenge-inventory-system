8c34050878e5c2612c490eaac5bf9662b1032959
Daniel Ha <audanielha@gmail.com>
2024-11-13 22:17:51 +1300

    add function to remove object based on name

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 let container = [
     {
-    "name" : "test",
+    "name" : "bottom",
     "quantity": 0,
     "price": 0
+    },
+    {
+    "name" : "top",
+    "quantity": 5,
+    "price": 10
     }
 ]
 
+
+// Add Product function
 let addProduct = (name, quantity, price)=>{
     container.push({name, quantity, price})
 }
-addProduct("test", 23, 23)
-console.log(container[1])
+
+// addProduct("test", 23, 23)
+
+// Remove Product function
+let removeProduct = (removeTarget) =>{
+    for(let i = 0; i < container.length; i++){
+        if(removeTarget === container[i].name){
+            delete container[i]
+            console.log(container)
+        }
+    }
+}
+removeProduct("bottom")
