6ae5697de7f09d287b59446ea5e75d1bd4afe02b
Daniel Ha <audanielha@gmail.com>
2024-11-15 19:21:30 +1300

    did tests to make sure functions are working

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
 let container = [
     {
-    "name" : "bottom",
-    "quantity": 10,
-    "price": 0
+    "name" : "first",
+    "quantity": 5,
+    "price": 10,
     },
     {
-    "name" : "top",
-    "quantity": 5,
-    "price": 10
+    "name" : "second",
+    "quantity": 10,
+    "price": 20,
+    },
+    {"name" : "third",
+    "quantity": 15,
+    "price": 30,
+    },
+    {"name" : "fourth",
+    "quantity": 20,
+    "price": 40,
     }
 ]
 
 
 // Add Product function
 let addProduct = (name, quantity, price)=>{
-    container.push({name, quantity, price})
+    container.push({name, quantity, price});
 }
-addProduct("test", 23, 23)
+addProduct("Five", 50, 25)
+addProduct("Six", 60, 30)
+addProduct("Seven", 70, 35)
 
 // Remove Product function
 let removeProduct = (removeTarget) =>{
@@ -26,18 +36,33 @@ let removeProduct = (removeTarget) =>{
         }
     }
 }
-removeProduct("bottom")
+//removeProduct("third");
 
-// Update Quantity
+// Update Quantity function
 let updateQuantity = (name,quantity)=>{
     for(let i = 0; i < container.length; i++){
         if(name === container[i].name){
-            console.log(container[i].quantity = quantity)
+            container[i].quantity = quantity;
         }
     }
 }
-updateQuantity("bottom", 2)
+updateQuantity("second", 2)
 
+// Calculate Total Value function calculateTotalValue()
+let calculateTotalValue = () =>{
+    let totalValue = 0;
+    for(let i = 0; i < container.length; i++){
+        totalValue += container[i].price;
+        console.log(container[i].price)  
+    }
+    console.log(totalValue)
+    return totalValue
+    
+}
 
+calculateTotalValue()
 
+for(let i = 0; i < container.length; i++){
+    console.log(container[i])
+}
 
